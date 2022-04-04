f314a22a13302409e1caa4606b45b640bfe8d5d7
Mohak Jain <mohakjain55055@gmail.com>
2022-04-04 11:55:15 +0530

    Score Added

---
+++ Ludum Dare 50/Assets/Code/UI/ScoreScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    GameObject Player;
+    float PreviousPosition;
+    float HighestPosition = 0f;
+    float Score = 0f;
+    Text Label;
+    void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        PreviousPosition = Player.transform.position.y;
+        Label = GetComponent<Text>();
+    }
+    private void LateUpdate()
+    {
+        if (PreviousPosition > HighestPosition)
+        {
+            HighestPosition = PreviousPosition;
+        }
+        if (HighestPosition < Player.transform.position.y)
+        {
+            //Increase the score
+            Score += 0.2f;
+            int DisplayScore = (int)Score;
+            Label.text = $"SCORE : {DisplayScore}";
+        }
+        PreviousPosition = Player.transform.position.y;
+    }
+}
